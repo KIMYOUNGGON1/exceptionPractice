b64130e79def4e22da92a00a1883bc40edfe0aba
KIMYOUNGON1 <dudrhs950@naver.com>
2024-05-22 13:00:35 +0900

    20240522 HandlerExceptionResolver

---
+++ Spring/exception /src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -0,0 +1,21 @@
+package hello.exception.resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+
+        if (ex instanceof IllegalAccessException) {
+            log.info("IllegalArgumentException resolver to 400");
+        }
+
+        return null;
+
+    }
+}
